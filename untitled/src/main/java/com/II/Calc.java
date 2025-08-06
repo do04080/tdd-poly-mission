be889f6596dc4f4248eb2a02dea7667babe0bdcd
[BE9]도석환 <do04080@gmail.com>
2025-08-06 12:13:48 +0900

    괄호 계산 구현 완료

--- untitled/src/main/java/com/II/Calc.java
+++ untitled/src/main/java/com/II/Calc.java
@@ -4,6 +4,40 @@ public class Calc {
     public static int run(String input) {
         String part = input.replace(" ", "");
 
+        for(int i = part.length()-1; i >=0; i--) {
+            char c = part.charAt(i);
+
+            if (c == '/') {
+                String left = part.substring(0, i);
+                String right = part.substring(i + 1);
+                return run(left) / run(right);
+            }
+        }
+        for (int i = part.length()-1; i >=0; i--) {
+            char c = part.charAt(i);
+            if (c == ')') {
+                int j = i;
+                int count = 1;
+                while (j >=0 &&count>0) {
+                    j--;
+                    if(part.charAt(j)==')'){
+                        count++;
+                    }
+                    if(part.charAt(j)=='('){
+                        count--;
+                    }
+                }
+                if (j < i) {
+                    if (j == 0 && i == part.length() - 1) {
+                        return run(part.substring(j + 1, i));
+                    }
+                    String innerPart = part.substring(j + 1, i);
+                    String left = part.substring(0, j);
+                    String right = part.substring(i + 1);
+                    return run(left + run(innerPart) + right);
+                }
+            }
+        }
 
         for (int i = part.length()-1; i >=0; i--) {
             char c = part.charAt(i);
@@ -15,6 +49,7 @@ public class Calc {
             }
 
         }
+
         for(int i = part.length()-1; i >=0; i--) {
             char c = part.charAt(i);
             if (c == '*' ) {
@@ -23,6 +58,9 @@ public class Calc {
                 return run(left) * run(right);
             }
         }
+
+
+
 
         return Integer.parseInt(part);
 
