bd576bf76f646f9a5811a3ded0fd192518edd5f0
[BE9]도석환 <do04080@gmail.com>
2025-08-06 11:01:53 +0900

    곱셈 구현 및 곱셈 먼저 계산 구현(음수 곱셈 미구현)

--- untitled/src/main/java/com/II/Calc.java
+++ untitled/src/main/java/com/II/Calc.java
@@ -4,14 +4,21 @@ public class Calc {
     public static int run(String input) {
         String part = input.replace(" ", "");
 
+
         for (int i = part.length()-1; i >=0; i--) {
             char c = part.charAt(i);
             if ((c == '+' || c == '-') && i > 0) {
                 String left = part.substring(0, i);
                 String right = part.substring(i + 1);
-                int leftNum = run(left);
-                int rightNum = run(right);
-                return c=='+'?leftNum + rightNum: leftNum - rightNum;
+                return c=='+'?run(left) + run(right): run(left) - run(right);
+            }
+        }
+        for(int i = part.length()-1; i >=0; i--) {
+            char c = part.charAt(i);
+            if (c == '*' ) {
+                String left = part.substring(0, i);
+                String right = part.substring(i + 1);
+                return run(left) * run(right);
             }
         }
 
